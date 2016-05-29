c2f9052675750be0cf0c2d593525deb4d2405b6c
Allan Inda <nalla@kariallan.com>
2016-05-28 22:53:49 -0500

    added debug level for the serial port logging

---
+++ debugprint.cpp
@@ -0,0 +1,188 @@
+/*
+ * debug.cpp
+ *
+ *  Created on: May 28, 2016
+ *      Author: allan
+ */
+
+#include "debugprint.h"
+
+bool DebugPrint::isDebugLevel(DebugLevel dlevel) {
+	if (dlevel == debuglevel || dlevel == DebugLevel::ALWAYS
+			|| static_cast<int>(debuglevel) >= static_cast<int>(dlevel)) {
+		return true;
+	}
+	return false;
+}
+
+void DebugPrint::incrementDebugLevel(void) {
+	int newd = static_cast<int>(debuglevel) + 1;
+	debuglevel = static_cast<DebugLevel>(newd);
+	if (debuglevel >= DebugLevel::END) {
+		debuglevel = DebugLevel::NONE;
+	}
+}
+
+String DebugPrint::getDebugLevelString(void) {
+	switch (debuglevel) {
+		case DebugLevel::ALWAYS:
+			return String("ALWAYS");
+			break;
+		case DebugLevel::NONE:
+			return String("NONE");
+			break;
+		case DebugLevel::INFO:
+			return String("INFO");
+			break;
+		case DebugLevel::ERROR:
+			return String("ERROR");
+			break;
+		case DebugLevel::DEBUG:
+			return String("DEBUG");
+			break;
+		case DebugLevel::DEBUGMORE:
+			return String("DEBUGMORE");
+			break;
+		default:
+			break;
+	}
+	return ("UNKNOWN");
+}
+
+void DebugPrint::println(DebugLevel dlevel, const String &s) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(s);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, const __FlashStringHelper* p) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(p);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, const char s[]) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(s);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, char c) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(c);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, unsigned char c, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(c, m);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, int n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(n, m);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, unsigned int n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(n, m);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, long n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(n, m);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, unsigned long n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(n, m);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, double n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(n, m);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel, const Printable& c) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println(c);
+	}
+}
+
+void DebugPrint::println(DebugLevel dlevel) {
+	if (isDebugLevel(dlevel)) {
+		Serial.println();
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, const __FlashStringHelper* p) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(p);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, const String &s) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(s);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, const char s[]) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(s);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, char c) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(c);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, unsigned char n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(n, m);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, int n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(n, m);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, unsigned int n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(n, m);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, long n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(n, m);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, unsigned long n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(n, m);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, double n, int m) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(n, m);
+	}
+}
+
+void DebugPrint::print(DebugLevel dlevel, const Printable& p) {
+	if (isDebugLevel(dlevel)) {
+		Serial.print(p);
+	}
+}
---
+++ debugprint.h
@@ -0,0 +1,62 @@
+/*
+ * debugprint.h
+ *
+ *  Created on: May 28, 2016
+ *      Author: allan
+ */
+
+#ifndef DEBUGPRINT_H_
+#define DEBUGPRINT_H_
+
+#include <Arduino.h>
+
+enum class DebugLevel
+	: int {
+		ALWAYS = 0, NONE, INFO, ERROR, DEBUG, DEBUGMORE, END
+};
+
+class DebugPrint {
+private:
+	DebugLevel debuglevel;
+
+public:
+	DebugPrint()
+			: debuglevel(DebugLevel::ERROR) {
+	}
+	DebugLevel getDebugLevel(void) {
+		return debuglevel;
+	}
+	void setDebugLevel(DebugLevel dlevel) {
+		debuglevel = dlevel;
+	}
+	bool isDebugLevel(DebugLevel dlevel);
+	void incrementDebugLevel(void);
+	String getDebugLevelString(void);
+	void println(DebugLevel dlevel, const __FlashStringHelper *);
+	void println(DebugLevel dlevel, const String &s);
+	void println(DebugLevel dlevel, const char[]);
+	void println(DebugLevel dlevel, char);
+	void println(DebugLevel dlevel, unsigned char, int = DEC);
+	void println(DebugLevel dlevel, int, int = DEC);
+	void println(DebugLevel dlevel, unsigned int, int = DEC);
+	void println(DebugLevel dlevel, long, int = DEC);
+	void println(DebugLevel dlevel, unsigned long, int = DEC);
+	void println(DebugLevel dlevel, double, int = 2);
+	void println(DebugLevel dlevel, const Printable&);
+	void println(DebugLevel dlevel);
+
+	void print(DebugLevel dlevel, const __FlashStringHelper *);
+	void print(DebugLevel dlevel, const String &s);
+	void print(DebugLevel dlevel, const char[]);
+	void print(DebugLevel dlevel, char);
+	void print(DebugLevel dlevel, unsigned char, int = DEC);
+	void print(DebugLevel dlevel, int, int = DEC);
+	void print(DebugLevel dlevel, unsigned int, int = DEC);
+	void print(DebugLevel dlevel, long, int = DEC);
+	void print(DebugLevel dlevel, unsigned long, int = DEC);
+	void print(DebugLevel dlevel, double, int = 2);
+	void print(DebugLevel dlevel, const Printable&);
+	void print(DebugLevel dlevel);
+};
+
+#endif /* DEBUGPRINT_H_ */
